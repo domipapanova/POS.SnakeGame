7ccf121ea623c89445053890e43da6ccbba30083
Dominika Papánová <dominika.papanova2@gmail.com>
2023-01-03 22:24:43 +0100

    final final final

--- main.cpp
+++ main.cpp
@@ -10,25 +10,10 @@
 int main () {
     srand(time(NULL));
 
-//    sf::RenderWindow window(sf::VideoMode(800, 600), "SnakeGame");
     Game* game = new Game(10, 5);
     game->start();
     game->stop();
     delete game;
 
-
-//    while (window.isOpen())
-//    {
-//        sf::Event event;
-//        while (window.pollEvent(event))
-//        {
-//            if (event.type == sf::Event::Closed)
-//                window.close();
-//        }
-//
-//        window.clear();
-//
-//        window.display();
-//    }
 }
 
