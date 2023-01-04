0ea47510a19a2eda6fccc595682d3d4556649464
Adam Nagy <aduxik@gmail.com>
2023-01-04 11:18:28 +0100

    second snake added

--- main.cpp
+++ main.cpp
@@ -1,34 +1,12 @@
-#include "Grid.h"
 #include "Game.h"
-#include "Snake.h"
-
 #include <iostream>
-#include <cstring>
-#include <SFML/Graphics.hpp>
-
 
 int main () {
     srand(time(NULL));
-
-//    sf::RenderWindow window(sf::VideoMode(800, 600), "SnakeGame");
-    Game* game = new Game(10, 5);
+    Game* game = new Game(12, 8);
     game->start();
     game->stop();
     delete game;
 
-
-//    while (window.isOpen())
-//    {
-//        sf::Event event;
-//        while (window.pollEvent(event))
-//        {
-//            if (event.type == sf::Event::Closed)
-//                window.close();
-//        }
-//
-//        window.clear();
-//
-//        window.display();
-//    }
 }
 
